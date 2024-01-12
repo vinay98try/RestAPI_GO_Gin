ff60f675f1f619a8bc30a99959502be6b543737c
Imvdark <113287680+vinay98try@users.noreply.github.com>
2024-01-12 18:03:35 +0530

    Update user.go
    // No need to define Id here because it already present in the GORM Model...!

--- PracticeGDB/models/user.go
+++ PracticeGDB/models/user.go
@@ -4,7 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	ID       int    `json:"id" gorm: "primary_key"`
+	// ID       int    `json:"id" gorm: "primary_key"` 	
+	// No need to define Id here because it already present in the GORM Model...!
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
